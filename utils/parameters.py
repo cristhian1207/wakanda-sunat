c7e75044f5a4a53823fcc0e208c6228d2411133b
Cristhian <cristhian@MacBook-Pro-de-13.local>
2019-05-07 22:16:55 -0500

    cOMMIT

--- utils/parameters.py
+++ utils/parameters.py
@@ -5,7 +5,7 @@ global CSV_SUNAT_RUC
 global SYSTEM_THREADS
 
 URL_SUNAT_RUC='http://www2.sunat.gob.pe/padron_reducido_ruc.zip'
-PATH_SUNAT_RUC='./files/'
+PATH_SUNAT_RUC='files/'
 ZIP_SUNAT_RUC=PATH_SUNAT_RUC+'ruc.zip'
 TXT_SUNAT_RUC_FN='padron_reducido_ruc.txt'
 TXT_SUNAT_RUC=PATH_SUNAT_RUC+TXT_SUNAT_RUC_FN
