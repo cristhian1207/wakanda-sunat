1724157da4ce468ed050aab8d6913b46949ee02b
Cristhian <cristhian@MacBook-Pro-de-13.local>
2019-05-07 22:06:41 -0500

    Fixed some issues

--- load_sunat_ruc.py
+++ load_sunat_ruc.py
@@ -14,14 +14,14 @@ if __name__ == "__main__":
     logging.getLogger().setLevel(logging.INFO)
     logging.basicConfig(format='%(asctime)s - %(levelname)-8s: %(message)s', datefmt='%d-%m-%Y %H:%M:%S')
 
-    # logging.info('Descargando padrón')
-    # file_management.download_file()
+    logging.info('Descargando padrón')
+    file_management.download_file()
 
-    # logging.info('Descomprimiendo padrón')
-    # file_management.unzip_file()
+    logging.info('Descomprimiendo padrón')
+    file_management.unzip_file()
 
-    # logging.info('Partiendo archivos')
-    # total_rows=file_management.split_file()
+    logging.info('Partiendo archivos')
+    total_rows=file_management.split_file()
 
     se=sunat_table_dao.find_available()
     setattr(se, 'locked', 1)
